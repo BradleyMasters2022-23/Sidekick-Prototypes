07eca4876c394fb5483643bea3869dacfdb65ae8
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2022-09-14 16:24:56 -0500

    Simple AI

--- Sidekick Prototype/Assets/Scripts/Enemy.cs
+++ Sidekick Prototype/Assets/Scripts/Enemy.cs
@@ -5,53 +5,81 @@ using UnityEngine;
 /// <summary>
 /// this is enemy lmao
 /// </summary>
-public class Enemy : MonoBehaviour
+public class Enemy : IDamagable
 {
+    [Range(0,1)]
     public float rotationSpeed;
     public float walkSpeed;
     public GameObject shotPrefab;
     public float shootTime;
 
-    public float t = 0;
-    public float currTime = 1;
+    [Tooltip("Range the enemy can attack from")]
+    public float attackRange;
+    [Tooltip("Range the enemy will try to stay around")]
+    public float idealRange;
+    [Tooltip("Threshold to move the enemy")]
+    public float moveThreshold;
+    private float currDist;
+
+    private float time = 0;
+    private float currTime = 1;
+    private GameObject p;
 
 
-
-    [Tooltip("hello val1")]
-    [SerializeField] private int testVal1;
-
-    /// <summary>
-    /// hello val2
-    /// </summary>
-    [SerializeField] private int testVal2;
 
     // Start is called before the first frame update
     void Start()
     {
         currTime = TimeManager.worldTime;
+        p = FindObjectOfType<PlayerControllerRB>().gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currTime = TimeManager.worldTime;
+        currDist = Vector3.Distance(p.transform.position, transform.position);
 
-        transform.Translate(transform.forward * walkSpeed * Time.deltaTime * currTime);
-        transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime * currTime);
-
-
-        if(t >= shootTime)
+        // Shoot every few seconds if in range
+        if (time >= shootTime && currDist  <= attackRange)
         {
             Shoot();
-            t = 0;
+            time = 0;
         }
-        else
+        else if(time < shootTime)
         {
-            t += Time.deltaTime * currTime;
+            time += Time.deltaTime * currTime;
         }
-        testVal1 = 2;
     }
 
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        // Get current time state
+        currTime = TimeManager.worldTime;
+
+        // Get direction of player
+        Vector3 direction = p.transform.position - transform.position;
+        float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+        // Rotate enemy towards player
+        float nextAng = Mathf.LerpAngle(transform.rotation.eulerAngles.y, angle, rotationSpeed * currTime);
+        transform.rotation = Quaternion.Euler(0, nextAng, 0);
+
+        // Move enemy
+        if (Mathf.Abs(currDist - idealRange) > moveThreshold)
+        {
+            if (currDist < idealRange)
+            {
+                transform.position += transform.forward * -1 * walkSpeed * Time.deltaTime * currTime;
+            }
+            else if (currDist > idealRange)
+            {
+                transform.position += transform.forward * walkSpeed * Time.deltaTime * currTime;
+            }
+        }
+    }
+    
     private void Shoot()
     {
         Instantiate(shotPrefab, transform.position, transform.rotation);
