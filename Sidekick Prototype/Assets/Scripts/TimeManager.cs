6489a52560670aa08334275736f81c7c7f25a1aa
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2022-09-15 14:04:46 -0500

    Revert "Main merge"
    This reverts commit 0f4528786c00efd0a13db6f4137f20e9c5e8f069.

--- Sidekick Prototype/Assets/Scripts/TimeManager.cs
+++ Sidekick Prototype/Assets/Scripts/TimeManager.cs
@@ -7,11 +7,16 @@ public class TimeManager : MonoBehaviour
 {
     public static float worldTime;
 
-    public float normalTime;
+    public bool toggle;
+
+    [HideInInspector] public float normalTime = 1;
+    [Range(0, 1)]
     public float slowedTime;
+    public float timeChangeSpeed;
 
     PlayerControls controller;
     InputAction slow;
+    InputAction slowHold;
 
     bool slowing;
     
@@ -21,40 +26,66 @@ public class TimeManager : MonoBehaviour
         slowing = false;
         StartTime();
 
-        slow = controller.Player.SlowTime;
-        slow.performed += ToggleSlow;
-        slow.Enable();
+        if(toggle)
+        {
+            slow = controller.Player.SlowTime;
+            slow.performed += ToggleSlow;
+            slow.Enable();
+        }
+        else
+        {
+            slowHold = controller.Player.HoldSlow;
+            slowHold.started += ToggleSlow;
+            slowHold.canceled += ToggleSlow;
+            slowHold.Enable();
+        }
+        
     }
+
 
     private void OnDisable()
     {
-        slow.Disable();
+        if(toggle)
+        {
+            slow.Disable();
+        }
+        else
+        {
+            slowHold.Disable();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (slowing && worldTime != slowedTime)
+        {
+            worldTime = Mathf.Lerp(worldTime, slowedTime, timeChangeSpeed);
+            if (worldTime < 0.05)
+                worldTime = 0;
+        }
+        else if (!slowing && worldTime != normalTime)
+        {
+            worldTime = Mathf.Lerp(worldTime, normalTime, timeChangeSpeed);
+            if (worldTime > 0.95)
+                worldTime = 1;
+        }
     }
 
     public void SlowTime()
     {
-        Debug.Log("Freezing Time");
+        //Debug.Log("Freezing Time");
         worldTime = slowedTime;
     }
 
     public void StartTime()
     {
-        Debug.Log("Starting Time");
+        //Debug.Log("Starting Time");
         worldTime = normalTime;
     }
 
     private void ToggleSlow(InputAction.CallbackContext ctx)
     {
         slowing = !slowing;
-
-        if(slowing)
-        {
-            StartTime();
-        }
-        else
-        {
-            SlowTime();
-        }
     }
 
 }
