239f0fb51b920deb2c43f5cd8fe9caab433e0130
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2022-10-13 14:16:54 -0500

    Base laser in
    Simple version of the laser projectile is in, and a test enemy made for it.

--- Sidekick Prototype/Assets/Scripts/Lasers.cs
+++ Sidekick Prototype/Assets/Scripts/Lasers.cs
@@ -2,77 +2,180 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lasers : MonoBehaviour
+public class Lasers : IRangeAttack
 {
-    [SerializeField] private int damage;
-    [SerializeField] private float lifeTime;
+    [Header("---Gameplay---")]
+    [Tooltip("How long does it hold its shot and show the 'flare' before firing")]
+    [SerializeField] private float fireDelay;
+    [Tooltip("How close to firing should this allow to fire early when time is frozen")]
+    [SerializeField] private float earlyFireThreshold;
+    [Tooltip("What is the cooldown for taking damage while standing inside the laser in frozen time")]
     [SerializeField] private float damageRate;
-    [SerializeField] private int maxDamageTick;
+    [Tooltip("What is the damage per tick when standing in laser in frozen time")]
+    [SerializeField] private float frozenDamageModifier;
+    [Tooltip("If missed, what is the default laser distance")]
     [SerializeField] private float defaultDistance;
-    [SerializeField] private Vector3 originPoint;
-    [SerializeField] private Vector3 aimDirection;
-
+    [Tooltip("What physics layer can this laser hit")]
     [SerializeField] private LayerMask hitLayers;
+    [Tooltip("What is the cooldown for taking damage while standing inside the laser in frozen time")]
+    [SerializeField] private float laserVisualOvershoot;
+    
+    [Header("---Audio---")]
+    [Tooltip("Sound played when starting to aim")]
+    [SerializeField] private AudioClip aimSound;
+    [Tooltip("Sound played when firing")]
+    [SerializeField] private AudioClip shootSound;
 
     private float lifeTimer;
-    private float damageTickTimer;
+    private float delayTimer;
+    private float damageRateTimer;
+    private bool hitTarget;
+    private bool fired;
 
+    /// <summary>
+    /// Visual line renderer for visuals
+    /// </summary>
+    private LineRenderer laserShot;
+    /// <summary>
+    /// Origin of the laser shot
+    /// </summary>
+    private Vector3 origin;
+    /// <summary>
+    /// Direction the laser shot fires
+    /// </summary>
+    private Vector3 direction;
 
     private void Start()
     {
-        GameObject p = FindObjectOfType<PlayerControllerRB>().gameObject;
-        Vector3 t = p.transform.position - transform.position;
+        hitTarget = false;
 
-        PrepareProjectile(transform.position, t.normalized);
+        GameObject p = FindObjectOfType<PlayerControllerRB>().gameObject;
+        if (aimSound != null)
+            AudioSource.PlayClipAtPoint(aimSound, FindObjectOfType<PlayerControllerRB>().transform.position, 1f);
+
+        Vector3 t = p.transform.position - transform.position;
+        laserShot = GetComponent<LineRenderer>();
+        laserShot.enabled = false;
+        PrepareProjectile(transform.position, transform.forward);
     }
 
-    public void PrepareProjectile(Vector3 origin, Vector3 direction)
+    /// <summary>
+    /// Load in the origin and direction
+    /// </summary>
+    /// <param name="_origin">origin point of this shot</param>
+    /// <param name="_direction">direction of this shot</param>
+    private void PrepareProjectile(Vector3 _origin, Vector3 _direction)
     {
-        RaycastHit hit;
-        //Debug.Log("told origin of " + origin);
+        origin = _origin;
+        direction = _direction;
+    }
 
+    /// <summary>
+    /// Fire the laser and prepare visuals and sounds
+    /// </summary>
+    private void FireLaser()
+    {
+        if (aimSound != null)
+            AudioSource.PlayClipAtPoint(shootSound, FindObjectOfType<PlayerControllerRB>().transform.position, 1f);
+
+        fired = true;
+        RaycastHit hit;
         transform.rotation = Quaternion.LookRotation(direction);
-        float distance;
+        laserShot.SetPosition(0, origin);
+
         if (Physics.Raycast(origin, direction, out hit, defaultDistance, hitLayers))
         {
-            distance = hit.distance;
+            laserShot.SetPosition(1, hit.point + direction * laserVisualOvershoot);
+            IDamagable target;
+            if(!hitTarget && hit.collider.TryGetComponent<IDamagable>(out target))
+            {
+                target.TakeDamage(damage);
+                hitTarget = true;
+            }
         }
         else
         {
-            distance = defaultDistance;
+            laserShot.SetPosition(1, origin + direction*defaultDistance);
         }
-        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, distance);
-        transform.position = origin + (direction.normalized * (transform.localScale.z / 2));
+
+        laserShot.enabled = true;
     }
 
+    /// <summary>
+    /// Fire constantly without visual indicator changes
+    /// </summary>
+    private void FireFrozenTime()
+    {
+        if(damageRateTimer >= damageRate)
+        {
+            hitTarget = false;
+            damageRateTimer = 0;
+        }
+        else
+        {
+            damageRateTimer += Time.deltaTime;
+        }
+
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(origin, direction, out hit, defaultDistance, hitLayers))
+        {
+            IDamagable target;
+            if (!hitTarget && hit.collider.TryGetComponent<IDamagable>(out target))
+            {
+                target.TakeDamage((int)(damage * frozenDamageModifier));
+                hitTarget = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Manage timers
+    /// </summary>
     private void Update()
     {
-        if(lifeTimer >= lifeTime)
+        if(!fired && delayTimer >= fireDelay)
+        {
+            FireLaser();
+        }
+        else if (!fired)
+        {
+            delayTimer += Time.deltaTime * TimeManager.worldTime;
+
+            // If within the threshold and time has frozen, then fire early anyways
+            if(TimeManager.worldTime == 0 && Mathf.Abs(fireDelay - delayTimer) <= earlyFireThreshold)
+            {
+                Debug.Log("Frozen threshold reached");
+                FireLaser();
+            }
+        }
+
+
+        if(fired && lifeTimer >= lifeTime)
         {
             DestroyLaser();
         }
-        else
+        else if(fired && TimeManager.worldTime > .8)
         {
             lifeTimer += Time.deltaTime * TimeManager.worldTime;
         }
+
+        if(fired && TimeManager.worldTime < 1)
+        {
+            FireFrozenTime();
+        }
     }
 
+    /// <summary>
+    /// Destroys the laser shot
+    /// </summary>
     private void DestroyLaser()
     {
         // Add additional effects for destroying the laser
+        laserShot.enabled = false;
         GetComponent<Collider>().enabled = false;
 
         Destroy(this.gameObject);
     }
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        other.GetComponent<IDamagable>().TakeDamage(damage);
-    //    }
-
-    //    Destroy(this.gameObject);
-    //}
-
 }
